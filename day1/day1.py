e0c05c72d72d94e45b63e39c615ab7b5711e684b
james.aldag <james.aldag@workday.com>
2020-11-30 22:12:04 -0800

    day1

---
+++ day1/day1.py
@@ -0,0 +1,15 @@
+
+file = open("data.txt", "r")
+
+values = []
+
+for x in file :
+	values.append(int(x))
+
+for x in range(len(values)) :
+	for y in range(x + 1, len(values)) :
+		if values[x] + values[y] == 2020 :
+			print "Part 1: " + str(values[x] * values[y])
+		for z in range(y + 1, len(values)) :
+			if values[x] + values[y] + values[z] == 2020 :
+				print "Part 2: " + str(values[x] * values[y] * values[z])
