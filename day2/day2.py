395d9a853ad4f5348c0091fedf9c6fa14927c5c4
james.aldag <james.aldag@workday.com>
2020-12-01 21:32:03 -0800

    day2

---
+++ day2/day2.py
@@ -0,0 +1,21 @@
+import re
+
+file = open("data.txt", "r")
+
+part1count = 0
+part2count = 0
+
+for x in file :
+	groups = re.search("^(\d+)-(\d+) (.): (.+)$", x)
+	low = int(groups.group(1))
+	high = int(groups.group(2))
+	char = groups.group(3)
+	password = groups.group(4)
+
+	if password.count(char) >= low and password.count(char) <= high :
+		part1count += 1
+	if password[low-1] == char and password[high-1] != char or password[low-1] != char and password[high-1] == char :
+		part2count += 1
+
+print "Part 1: " + str(part1count)
+print "Part 2: " + str(part2count)
