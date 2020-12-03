770ffed2001e7be8dffd8dc7853d44682af488d7
james.aldag <james.aldag@workday.com>
2020-12-02 21:42:56 -0800

    day3

--- day3/day3.py
+++ day3/day3.py
@@ -1,3 +1,4 @@
+import numpy
 file = open("data.txt", "r")
 
 idx = [0, 0, 0, 0, 0]
@@ -6,11 +7,11 @@ incrementor = [1, 3, 5, 7, .5]
 
 for x in file :
 	x = x.strip()
-	for i in range(5) :
+	for i in range(len(idx)) :
 		if x[int(idx[i])] == '#' and idx[i] == int(idx[i]):
 			trees[i] += 1
 		idx[i] += incrementor[i]
 		if idx[i] >= len(x) :
 			idx[i] = idx[i] - len(x)
 print trees
-print str(trees[0] * trees[1] * trees[2] * trees[3] * trees[4])
+print numpy.prod(trees)
