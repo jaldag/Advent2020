5b56551bda1a046ca03edc7e4c4b9a835d2f9832
james.aldag <james.aldag@workday.com>
2020-12-08 22:49:57 -0800

    day9

--- day9/day9.py
+++ day9/day9.py
@@ -1,46 +1,30 @@
 file = [int(l.strip()) for l in open("data.txt")]
 
-preamble = 25
-nums = list()
-
-for x in range(preamble) :
-	nums.append(file[x])
-
-idx = preamble
+idx = 25
 p1 = 0
 
 while True :
-	brk = False
-	for x in range(idx - preamble, len(nums) - 1) :
-		for y in range(x + 1, len(nums)) :
+	brk = True
+	for x in range(idx - 25, idx - 1) :
+		for y in range(x + 1, idx) :
 			if file[x] + file[y] == file[idx] :
 				idx += 1
-				nums.append(file[idx])
-				brk = True
+				brk = False
 				break
-		if brk :
+		if not brk :
 			break
-	if brk == False :
+	if brk:
 		p1 = file[idx]
 		break
-	else :
-		brk = False
 
 print "P1: " + str(p1)
 
 for x in range (idx - 1) :
 	brk = False
 	for y in range(x + 1, idx) :
-		val = 0
-		for z in range(x, y + 1) :
-			val += file[z]
+		val = sum(file[x:y+1])
 		if val == p1 :
-			low = float('inf')
-			high = 0
-			for v in range(x, y+1) :
-				low = min(low, file[v])
-				high = max(high, file[v])
-			print "P2: " + str(low + high)
+			print "P2: " + str(min(file[x:y+1]) + max(file[x:y+1]))
 			brk = True
 		if val > p1 :
 			break
