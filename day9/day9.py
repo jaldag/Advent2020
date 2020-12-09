b1817f3bc217159cec521c020727e649725158d5
james.aldag <james.aldag@workday.com>
2020-12-08 23:10:21 -0800

    day9

--- day9/day9.py
+++ day9/day9.py
@@ -5,24 +5,20 @@ p1 = 0
 
 while True :
 	brk = True
-	for x in range(idx - 25, idx - 1) :
+	for x in range(idx - 25, idx) :
 		if file[idx] - file[x] in file[x+1:idx] :
 			idx += 1
 			brk = False
 	if brk:
 		p1 = file[idx]
 		break
-
 print "P1: " + str(p1)
 
 for x in range (idx - 1) :
-	brk = False
 	for y in range(x + 1, idx) :
 		val = sum(file[x:y+1])
 		if val == p1 :
 			print "P2: " + str(min(file[x:y+1]) + max(file[x:y+1]))
-			brk = True
+			quit()
 		if val > p1 :
 			break
-	if brk :
-		break
